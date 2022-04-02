22e8436cca5cbdf42a85947dfbd3db0d7d599672
Mikkel <eaamiby@students.eaaa.dk>
2022-04-02 16:34:11 +0200

    Oprydning i kode + tilføjelse af pagination-api

--- MiniprojektAPI/Data/ApplicationDbContext.cs
+++ MiniprojektAPI/Data/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-using Model;
+using Models;
 
 namespace Data
 {
--- MiniprojektAPI/Service/DataService.cs
+++ MiniprojektAPI/Service/DataService.cs
@@ -3,25 +3,189 @@ using System.Text.Json;
 using System;
 
 using Data;
-using Model;
+using Models;
 
 namespace Service;
 
 public class DataService
 {
-	private ApplicationDbContext db { get; }
+    private ApplicationDbContext db { get; }
 
-	public DataService(ApplicationDbContext db) {
-		this.db = db;
+    public DataService(ApplicationDbContext db) {
+        this.db = db;
     }
+
+    // ---------------------------------------------------------------
+    // -- Questions --
+
+    public async Task<List<Question>> ListQuestions() {
+        return await db.Questions
+            .Include(q => q.Subject)
+            .ToListAsync();
+    }
+
+    public async Task<List<Question>> GetQuestionsByPage(int pageNumber, int pageSize) {
+        return await db.Questions
+            .Include(q => q.Subject)
+            .OrderByDescending(q => q.Date)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+    }
+
+    public async Task<List<Question>> ListQuestionsByNewest() {
+        return await db.Questions
+            .Include(q => q.Subject)
+            .OrderByDescending(q => q.Date)
+            .ToListAsync();
+    }
+
+    public async Task<Question> GetQuestionById(int questionId) {
+        return await db.Questions
+            .Where(q => q.Id == questionId)
+            .Include(q => q.Subject)
+            .FirstAsync();
+    }
+
+    public async Task<List<Question>> ListQuestionsBySubjectId(int subjectId) {
+        Subject subject = await db.Subjects
+            .Where(s => s.Id == subjectId)
+            .FirstAsync();
+
+        return db.Questions
+            .Where(q => q.Subject == subject)
+            .ToList();
+    }
+
+    public async Task<string> CreateQuestion(int subjectId, string title, string text, string username) {
+        Subject subject = await db.Subjects
+            .Where(s => s.Id == subjectId)
+            .FirstAsync();
+
+        Question question = new(subject, title, text, username, DateTime.Now)
+        {
+            Upvote = 1
+        };
+
+        await db.Questions.AddAsync(question);
+        await db.SaveChangesAsync();
+
+        return JsonSerializer.Serialize(
+            new { msg = "New question created", newQuestion = question.Title }
+        );
+    }
+
+    public async Task<string> UpdateQuestionByIdUpvote(int questionId) {
+        Question question = await db.Questions
+            .Where(q => q.Id == questionId)
+            .FirstAsync();
+
+        question.Upvote++;
+
+        db.Questions.Update(question);
+        await db.SaveChangesAsync();
+
+        return JsonSerializer.Serialize(
+            new { msg = "Question upvoted", updatedQuestion = question.Title }
+        );
+    }
+
+    public async Task<string> UpdateQuestionByIdDownvote(int questionId) {
+        Question question = await db.Questions
+            .Where(q => q.Id == questionId)
+            .FirstAsync();
+
+        question.Downvote++;
+
+        db.Questions.Update(question);
+        await db.SaveChangesAsync();
+
+        return JsonSerializer.Serialize(
+            new { msg = "Question downvoted", updatedQuestion = question.Title }
+        );
+    }
+
+    // ---------------------------------------------------------------
+    // -- Subjects --
+
+    public async Task<List<Subject>> ListSubjects() {
+        return await db.Subjects
+            .ToListAsync();
+    }
+
+    public async Task<Subject> GetSubjectById(int subjectId) {
+        return await db.Subjects
+            .Where(s => s.Id == subjectId)
+            .FirstAsync();
+    }
+
+    // ---------------------------------------------------------------
+    // -- Answers --
+
+    public async Task<List<Answer>> ListAnswersByQuestionId(int questionId) {
+        return await db.Questions
+            .Where(q => q.Id == questionId)
+            .SelectMany(a => a.Answers)
+            .ToListAsync();
+    }
+
+    public async Task<string> CreateAnswer(int questionId, string text, string username) {
+        Question question = await db.Questions
+            .Where(q => q.Id == questionId)
+            .FirstAsync();
+
+        Answer answer = new(question, text, username, DateTime.Now)
+        {
+            Upvote = 1
+        };
+
+        await db.Answers.AddAsync(answer);
+        await db.SaveChangesAsync();
+
+        return JsonSerializer.Serialize(
+            new { msg = "New answer created", newAnswer = answer.Text }
+        );
+    }
+
+    public async Task<string> UpdateAnswerByIdUpvote(int answerId) {
+        Answer answer = await db.Answers
+            .Where(a => a.Id == answerId)
+            .FirstAsync();
+
+        answer.Upvote++;
+
+        db.Answers.Update(answer);
+        await db.SaveChangesAsync();
+
+        return JsonSerializer.Serialize(
+            new { msg = "Answer upvoted", updatedAnswer = answer.Text }
+        );
+    }
+
+    public async Task<string> UpdateAnswerByIdDownvote(int answerId) {
+        Answer answer = await db.Answers
+            .Where(a => a.Id == answerId)
+            .FirstAsync();
+
+        answer.Downvote++;
+
+        db.Answers.Update(answer);
+        await db.SaveChangesAsync();
+
+        return JsonSerializer.Serialize(
+            new { msg = "Answer downvoted", updatedAnswer = answer.Text }
+        );
+    }
+
+    // ---------------------------------------------------------------
 
     /// <summary>
     /// Seeder noget nyt data i databasen hvis det er nødvendigt.
     /// </summary>
-	public void SeedData()
-	{
+	public void SeedData() {
         Subject subject = db.Subjects.FirstOrDefault()!;
-        if (subject == null) {
+        if (subject == null)
+        {
             subject = new Subject("Teknisk");
             db.Subjects.Add(subject);
             db.Subjects.Add(new Subject("Hjælp"));
@@ -45,165 +209,4 @@ public class DataService
 
         db.SaveChanges();
     }
-
-    // ---------------------------------------------------------------
-    // -- Questions --
-
-    public List<Question> ListQuestions() {
-		return db.Questions
-            .Include(q => q.Subject)
-		    .ToList();
-	}
-
-    public List<Question> ListQuestionsByNewest()
-    {
-        return db.Questions
-            .Include(q => q.Subject)
-            .OrderByDescending(q => q.Date)
-            .ToList();
-    }
-
-    public Question GetQuestionById(int questionId) {
-        return db.Questions
-            .Where(q => q.Id == questionId)
-            .Include(q => q.Subject)
-            .First();
-    }
-
-    public List<Question> ListQuestionsBySubjectId(int subjectId) {
-        Subject subject = db.Subjects
-            .Where(s => s.Id == subjectId)
-            .First();
-
-        return db.Questions
-            .Where(q => q.Subject == subject)
-            .ToList();
-    }
-
-    public string CreateQuestion(int subjectId, string title, string text, string username)
-    {
-        Subject subject = db.Subjects
-            .Where(s => s.Id == subjectId)
-            .First();
-
-        Question question = new(subject, title, text, username, DateTime.Now)
-        {
-            Upvote = 1
-        };
-
-        db.Questions.Add(question);
-        db.SaveChanges();
-
-        return JsonSerializer.Serialize(
-            new { msg = "New question created", newQuestion = question.Title }
-        );
-    }
-
-    public string UpdateQuestionByIdUpvote(int questionId)
-    {
-        Question question = db.Questions
-            .Where(q => q.Id == questionId)
-            .First();
-
-        question.Upvote++;
-
-        db.Questions.Update(question);
-        db.SaveChanges();
-
-        return JsonSerializer.Serialize(
-            new { msg = "Question upvoted", updatedQuestion = question.Title }
-        );
-    }
-
-    public string UpdateQuestionByIdDownvote(int questionId)
-    {
-        Question question = db.Questions
-            .Where(q => q.Id == questionId)
-            .First();
-
-        question.Downvote++;
-
-        db.Questions.Update(question);
-        db.SaveChanges();
-
-        return JsonSerializer.Serialize(
-            new { msg = "Question downvoted", updatedQuestion = question.Title }
-        );
-    }
-
-    // ---------------------------------------------------------------
-    // -- Subjects --
-
-    public List<Subject> ListSubjects() {
-        return db.Subjects
-            .ToList();
-    }
-
-    public Subject GetSubjectById(int subjectId) {
-        return db.Subjects
-            .Where(s => s.Id == subjectId)
-            .First();
-    }
-
-    // ---------------------------------------------------------------
-    // -- Answers --
-
-    public List<Answer> ListAnswersByQuestionId(int questionId) {
-        return db.Questions
-            .Where(q => q.Id == questionId)
-            .SelectMany(a => a.Answers)
-            .ToList();
-    }
-
-    public string CreateAnswer(int questionId, string text, string username) {
-        Question question = db.Questions
-            .Where(q => q.Id == questionId)
-            .First();
-
-        Answer answer = new(question, text, username, DateTime.Now)
-        {
-            Upvote = 1
-        };
-
-        db.Answers.Add(answer);
-        db.SaveChanges();
-
-        return JsonSerializer.Serialize(
-            new { msg = "New answer created", newAnswer = answer.Text }
-        );
-    }
-
-    public string UpdateAnswerByIdUpvote(int answerId)
-    {
-        Answer answer = db.Answers
-            .Where(a => a.Id == answerId)
-            .First();
-
-        answer.Upvote++;
-
-        db.Answers.Update(answer);
-        db.SaveChanges();
-
-        return JsonSerializer.Serialize(
-            new { msg = "Answer upvoted", updatedAnswer = answer.Text }
-        );
-    }
-
-    public string UpdateAnswerByIdDownvote(int answerId)
-    {
-        Answer answer = db.Answers
-            .Where(a => a.Id == answerId)
-            .First();
-
-        answer.Downvote++;
-
-        db.Answers.Update(answer);
-        db.SaveChanges();
-
-        return JsonSerializer.Serialize(
-            new { msg = "Answer downvoted", updatedAnswer = answer.Text }
-        );
-    }
-
-    // ---------------------------------------------------------------
 }
