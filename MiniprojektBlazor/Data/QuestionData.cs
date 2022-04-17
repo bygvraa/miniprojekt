3d1af7092919e99749b36a783d75840db778e570
Mikkel <eaamiby@students.eaaa.dk>
2022-04-17 15:37:41 +0200

    Tilføjet data validation for QuestionData og AskQuestionPage

--- MiniprojektBlazor/Data/QuestionData.cs
+++ MiniprojektBlazor/Data/QuestionData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Data
 {
@@ -6,9 +7,23 @@ namespace Data
     {
         // Properties
         public int Id { get; set; }
+
+        [ValidateComplexType]
         public SubjectData Subject { get; set; }
+
+        [Required]
+        [Display(Name = "Overskrift")]
+        [StringLength(150, ErrorMessage = "Overskriften skal være på mellem {2} og {1} tegn.", MinimumLength = 2)]
         public string Title { get; set; }
+
+        [Required]
+        [Display(Name ="Spørgsmål")]
+        [StringLength(500, ErrorMessage = "Sprøgsmålet skal være på mellem {2} og {1} tegn.", MinimumLength = 2)]
         public string Text { get; set; }
+
+        [Required]
+        [Display(Name = "Navn")]
+        [StringLength(20, ErrorMessage = "Navnet er skal være mellem {2} og {1} bogstaver langt.", MinimumLength = 2)]
         public string Username { get; set; }
         public DateTime Date { get; set; }
         public int Upvote { get; set; }
